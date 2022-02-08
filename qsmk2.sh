14dca56bb446912da87e00a72cdbb41df23835b1
Koen Veys <koen@dirac2>
2022-02-08 16:33:22 +0100

    removed zsh-part

--- qsmk2.sh
+++ qsmk2.sh
@@ -5,9 +5,6 @@
 [[ "$vnum" == "a"* ]] && vnum=99
 #TODO: add p-compatibility
 
-#bash-version doesn't work atm.. --> Fixed!?
-#put as default!
-if [ -n "$BASH_VERSION" ]; then
 vstat=($(qstat -u `whoami`| tail -n+6))
 vnode="gx"
 nnode=2
@@ -35,31 +32,3 @@ done
 
 echo "Used: ${nodelist[@]}"
 echo "Free: ${freelist[@]}"
-
-#DEPRECATED
-elif [ -n "$ZSH_VERSION" ]; then
-vstat=($(qstat -u koen| tail -n+6))
-vnode="gx"
-nnode=3
-nodelist=()
-until [[ $vnode == "" ]]; do
-    vnode=${vstat[$nnode]}
-    nnode=$(($nnode+11))
-    if [[ ! " ${nodelist[*]} " == *" $vnode "* ]]; then
-      nodelist+=($vnode)
-    fi
-done
-vartot=($(qsum -u | grep -A $(($vnum+5)) 'free' | tail -n $(($vnum+5)) ))
-#should maybe be increased to 6 or 7.. if for some reason an error occurs
-
-# get the first elements with a g AND not part of nodelist
-freelist=()
-i=1
-until [[ $(($#freelist[@] + $#nodelist[@])) == $vnum ]]; do
-    [[ ${vartot[$i]} == "g"* ]] && [[ ! " ${nodelist[*]} " == *" ${vartot[$i]} "* ]] && freelist+=(${vartot[$i]})
-    i=$(($i+1))
-done
-
-echo "Used: ${nodelist[*]}"
-echo "Free: ${freelist[*]}"
-fi
