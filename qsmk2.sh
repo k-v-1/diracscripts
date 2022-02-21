f7840643a28c286cac689ddac097b8ec4f8f678f
Koen Veys <koen@dirac2>
2022-02-21 09:59:41 +0100

    qsmk2: Added p-functionality with -p option as first arg

--- qsmk2.sh
+++ qsmk2.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ## qsmk2.sh
 # gives available g-nodes, based on qsm and qst
+# added p-functionality with gp-variable
+
+#arg-parsing: -p: give p-nodes instead of g
+#           arg2: total number of nodes to print, or 'a'=all available nodes
+gp=g
+[[ "$1" == "-p" ]] && gp=p && shift
 [[ $# == 0 ]] && vnum=5 || vnum=$1
 [[ "$vnum" == "a"* ]] && vnum=99
-#TODO: add p-compatibility
 
+#check which nodes are free with qstat, should work across users due to 'whoami'
 vstat=($(qstat -u `whoami`| tail -n+6))
 vnode="gx"
 nnode=2
@@ -17,16 +23,17 @@ until [[ $vnode == "" ]]; do
     fi
 done
 
-vartot=($(qsum -gu | grep -A $(($vnum+5)) 'free' | tail -n $(($vnum+5)) ))
-#should maybe be increased to 6 or 7.. if for some reason an error occurs
+#get list of free nodes, based on user availability due to -u flag?
+vartot=($(qsum -${gp}u | grep -A $(($vnum+5)) 'free' | tail -n $(($vnum+5)) ))
+#should maybe be increased to 6 or 7.. if for some reason an error occurs --> so far not necessary
 
-# get the first elements with a g AND not part of nodelist
+# get the first elements with a g/p AND not part of nodelist or forbidden list (g1 and g2)
 # loops stops when vartot is finished
 freelist=()
 i=0
 until [[ $((${#freelist[@]} + ${#nodelist[@]})) -ge $vnum ]]; do
     [[ "x${vartot[$i]}" == "x" ]] && break
-    [[ ${vartot[$i]} == "g"* ]] && [[ ! " ${nodelist[@]} g1 g2 " == *" ${vartot[$i]} "* ]] && freelist+=(${vartot[$i]})
+    [[ ${vartot[$i]} == "${gp}"* ]] && [[ ! " ${nodelist[@]} g1 g2 " == *" ${vartot[$i]} "* ]] && freelist+=(${vartot[$i]})
     i=$(($i+1))
 done
 
