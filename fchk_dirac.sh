90334032dc4cca9dc61731d78bf5b37181f723b7
Koen Veys <koen@dirac2>
2022-02-22 13:29:56 +0100

    bugfix

--- fchk_dirac.sh
+++ fchk_dirac.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+[[ $# -eq 0 ]] || [[ "$1" == "-h" ]] && echo "give chk-file (relative to pwd)" && exit 0
 
 names=()
 for chk in "$@"; do
 [[ "$chk" != *".chk" ]] && echo "Warning: file has no chk-extension? Continue in 5s" && sleep 5
-names+=`pwd`/$chk
+names+=" `pwd`/$chk"
 done
 echo "${names[@]}"
+echo
 exec /bin/sh -s <<EOF
-for fl in "${names[@]}"; do
+for fl in ${names[@]}; do
 /usr/local/chem/g16A03/formchk \$fl
 done
 EOF
