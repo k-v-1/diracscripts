039cd0d9bbb695658361124606337fb147acf12e
Koen Veys <koen@dirac2>
2022-02-22 11:56:03 +0100

    use bash instead of zsh for fchk

--- fchk_dirac.sh
+++ fchk_dirac.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
 names=()
 for chk in "$@"; do
 [[ "$chk" != *".chk" ]] && echo "Warning: file has no chk-extension? Continue in 5s" && sleep 5
 names+=`pwd`/$chk
 done
-echo $names
+echo "${names[@]}"
 exec /bin/sh -s <<EOF
-for fl in $names; do
+for fl in "${names[@]}"; do
 /usr/local/chem/g16A03/formchk \$fl
 done
 EOF
